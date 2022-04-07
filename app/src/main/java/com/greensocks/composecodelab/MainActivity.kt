c22bc83516416b88ddec941266a1488b869908d0
Ionut <isalavastru@wehkamp.nl>
2022-04-07 10:38:17 +0300

    persist lazy column state.

--- app/src/main/java/com/greensocks/composecodelab/MainActivity.kt
+++ app/src/main/java/com/greensocks/composecodelab/MainActivity.kt
@@ -3,14 +3,12 @@ package com.greensocks.composecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,22 +28,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun MyApp() {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
     if (shouldShowOnboarding) {
-        OnboardingScreen(onContinueClicked = {shouldShowOnboarding = false})
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
     } else {
         Greetings()
     }
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("Snarf", "Slurmp")) {
-    Column(
-        modifier = Modifier
-            .background(color = MaterialTheme.colors.error)
-            .padding(vertical = 4.dp)
-    ) {
-        for (name in names) {
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name)
         }
     }
@@ -77,7 +71,7 @@ private fun Greeting(name: String) {
 }
 
 @Composable
-fun OnboardingScreen(onContinueClicked:()->Unit) {
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
 
     Surface {
         Column(
