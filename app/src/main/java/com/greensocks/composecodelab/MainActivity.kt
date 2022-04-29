cd2fd55172e1867a6f9beeb6310b40d345f29ed1
Ionut <isalavastru@wehkamp.nl>
2022-04-29 11:38:18 +0300

    add a crash after animating expanding animation.

--- app/src/main/java/com/greensocks/composecodelab/MainActivity.kt
+++ app/src/main/java/com/greensocks/composecodelab/MainActivity.kt
@@ -3,6 +3,9 @@ package com.greensocks.composecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -47,8 +50,14 @@ private fun Greetings(names: List<String> = List(1000) { "$it" }) {
 
 @Composable
 private fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioHighBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -62,8 +71,8 @@ private fun Greeting(name: String) {
                 Text("Hello, ")
                 Text("$name!")
             }
-            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(if (expanded.value) "Show Less" else "Show More")
+            OutlinedButton(onClick = { expanded = !expanded }) {
+                Text(if (expanded) "Show Less" else "Show More")
             }
         }
 
